aacf999f65944a381491bed17b4b791799538d26
Nidhish Rathore <codenidhish07@gmail.com>
2024-11-11 23:29:18 +0530

    fix

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 connectDB();
 
-app.use(cors);
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(bodyParser.json())
